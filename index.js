6d70314c5dcdc110773a2f41b073bd6f3d2ec778
Stanislav Mitkin <mitkinstas@gmail.com>
2024-04-04 14:13:13 +0300

    add fastify

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+import { fastify } from "fastify";
 import { Telegraf } from "telegraf";
-import { message } from "telegraf/filters";
 
 const TOKEN = "6748836434:AAGMcz5CB8ACnpUpTnJenvBPvkQ1g14hwvE";
 const DOMAIN = "telegrafbot.vercel.app";
 
 const bot = new Telegraf(TOKEN);
+const app = fastify();
 
-bot.on(message("text"), (ctx) => ctx.reply("Hello"));
+const webhook = await bot.createWebhook({ domain: DOMAIN });
 
-// Start webhook via launch method (preferred)
-bot.launch({
-  webhook: {
-    domain: DOMAIN,
-    port: 8080,
-  },
-});
+app.post(`/telegraf/${bot.secretPathComponent()}`, webhook);
+
+bot.on("text", (ctx) => ctx.reply("Hello"));
+
+app.listen({ port: port }).then(() => console.log("Listening on port", port));
