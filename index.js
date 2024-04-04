0d4cb4d9dc59ff5c320719ac344b5bcd64e9e236
Stanislav Mitkin <mitkinstas@gmail.com>
2024-04-04 14:29:59 +0300

    add port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Telegraf } from "telegraf";
 
 const TOKEN = "6748836434:AAGMcz5CB8ACnpUpTnJenvBPvkQ1g14hwvE";
 const DOMAIN = "telegrafbot.vercel.app";
+const PORT = 8080;
 
 const bot = new Telegraf(TOKEN);
 const app = fastify();
@@ -13,4 +14,4 @@ app.post(`/telegraf/${bot.secretPathComponent()}`, webhook);
 
 bot.on("text", (ctx) => ctx.reply("Hello"));
 
-app.listen({ port: port }).then(() => console.log("Listening on port", port));
+app.listen({ port: PORT }).then(() => console.log("Listening on port", PORT));
